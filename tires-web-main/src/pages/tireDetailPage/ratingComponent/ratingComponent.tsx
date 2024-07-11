2be8f14e4157245f9e47e55ec9d21a47060f8175
Sergey <141022302+ApertureFacility@users.noreply.github.com>
2024-07-11 12:08:09 +0300

    Update ratingComponent.tsx

--- tires-web-main/src/pages/tireDetailPage/ratingComponent/ratingComponent.tsx
+++ tires-web-main/src/pages/tireDetailPage/ratingComponent/ratingComponent.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './ratingComponents.css'
+import './ratingComponents.css';
 
 const Rating: React.FC<{ label: string, stars: number }> = ({ label, stars }) => (
   <div className="rating">
     <div className="rating__label">{label}</div>
     <div className="rating__stars">
       {Array(stars).fill(0).map((_, i) => (
-        <img key={i} src='/public/star.svg' alt="Rating star" />
+        <img key={i} src='/star.svg' alt="Rating star" />
       ))}
     </div>
   </div>
 );
+
 export default Rating;
